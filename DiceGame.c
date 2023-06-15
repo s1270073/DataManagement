d15a015b20b0503ec36da1b157b7a681bee7aeb6
s1270073 <s1270073@oflsmdc2.u-aizu.ac.jp>
2023-06-15 17:46:48 +0900

    Won/lost message added

--- DiceGame.c
+++ DiceGame.c
@@ -7,14 +7,21 @@ int main(){
 
   printf("What is your name?\n> ");
   scanf("%s",name);
-  printf("Hello, %s!",name);
+  printf("Hello, %s!\n",name);
   printf("Rolling dice...\n");
   printf("Die 1: ");
   scanf("%d",&die1);
   printf("Die 2: ");
   scanf("%d",&die2);
   total=die1+die2;
-  printf("Total value: %d",total);
+  printf("Total value: %d \n",total);
+  if(total>7){
+    printf("You won ");
+  }
+  else{
+    printf("You lost ");
+  }
+  
   printf("\n");
 
   return 0;
